c014d71cd7df5cbe05ff92c3434364c211c8d032
adisharma9120 <aditya91sharma73619@gmail.com>
2026-05-16 06:44:10 +0000

    first push

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main(){
-    println!("Hello, World");
-}
+ use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input the number you want to guess.");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("You guessed: {}", guess);
+} 
+
+ 
